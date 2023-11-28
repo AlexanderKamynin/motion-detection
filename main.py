91e26e9e02ab726117a0130438fb7621ca92fc23
AlexanderKamynin <kamyninsasa@gmail.com>
2023-11-28 14:26:08 +0300

    reading video stream

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-import numpy as np
+# import dependencies
+import cv2
+
+# import project modules
+from src.detection.openCV.detectionCV import MotionDetectionCV
+
+class Core:
+    def __init__(self):
+        self.__video_stream = None
+        self.__motion_detection_cv = None
+        
+    def start(self):
+        self.__read_video()
+        self.__motion_detection_cv = MotionDetectionCV(self.__video_stream)
+        
+        self.__motion_detection_cv.detect()
+    
+    def __read_video(self):
+        print('Start read the video...')
+        self.__video_stream = cv2.VideoCapture('src/videos/default.mp4')
+        if(self.__video_stream == False):
+            raise ValueError('Error opening video stream')
 
 
 if __name__ == '__main__':
-	my_array = np.ndarray([1,2,3])
-	print(my_array)
+    core = Core()
+    core.start()
---
+++ src/detection/openCV/detectionCV.py
@@ -0,0 +1,13 @@
+import cv2
+
+class MotionDetectionCV:
+    def __init__(self, video_stream):
+        self.__video_stream = video_stream
+    
+    def detect(self):
+        #is_success, frame = 
+        pass
+    
+    
+if __name__ == '__main__':
+    pass
